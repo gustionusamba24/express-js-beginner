77775ba7ef7d034da94b69eb830ba2aa6d7d87ff
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-08-23 10:04:23 +0700

    Get method

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ const app = express();
 const PORT = 4000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/groceries", (req, res) => {
+  res.send([
+    {
+      item: "milk",
+      qty: 2,
+    },
+    {
+      item: "cereal",
+      qty: 3,
+    },
+    {
+      item: "meat",
+      qty: 1,
+    },
+  ]);
+});
