bce72c3eef093e97701a5e5081c473aa9b06bf7d
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-08-23 11:12:46 +0700

    Post method

--- src/index.js
+++ src/index.js
@@ -2,21 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+app.use(express.json());
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+const grocertList = [
+  {
+    item: "milk",
+    qty: 2,
+  },
+  {
+    item: "cereal",
+    qty: 3,
+  },
+  {
+    item: "meat",
+    qty: 1,
+  },
+];
+
 app.get("/groceries", (req, res) => {
-  res.send([
-    {
-      item: "milk",
-      qty: 2,
-    },
-    {
-      item: "cereal",
-      qty: 3,
-    },
-    {
-      item: "meat",
-      qty: 1,
-    },
-  ]);
+  res.send(grocertList);
+});
+
+app.post("/groceries", (req, res) => {
+  console.log(req.body);
+  grocertList.push(req.body);
+  res.send(201);
 });
