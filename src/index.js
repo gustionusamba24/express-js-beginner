0b71a4f132d12a07e3ff7df855cf6783eec6a8a9
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-08-24 12:57:39 +0700

    Middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const PORT = 4000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.url}`);
+  next();
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const grocertList = [
@@ -28,5 +33,5 @@ app.get("/groceries", (req, res) => {
 app.post("/groceries", (req, res) => {
   console.log(req.body);
   grocertList.push(req.body);
-  res.send(201);
+  res.sendStatus(201);
 });
