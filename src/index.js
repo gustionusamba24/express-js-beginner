3f7fb972732db81b4b6e1501ef32fde3963f4b64
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-08-25 20:28:26 +0700

    Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+const groceriesRouter = require("./routes/groceries");
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -9,35 +11,6 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/api/v1/groceries", groceriesRouter);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const groceryList = [
-  {
-    item: "milk",
-    qty: 2,
-  },
-  {
-    item: "cereal",
-    qty: 3,
-  },
-  {
-    item: "meat",
-    qty: 1,
-  },
-];
-
-app.get("/groceries", (req, res) => {
-  res.send(groceryList);
-});
-
-app.get("/groceries/:item", (req, res) => {
-  const { item } = req.params;
-  const groceryItem = groceryList.find((g) => g.item === item);
-  res.send(groceryItem);
-});
-
-app.post("/groceries", (req, res) => {
-  console.log(req.body);
-  grocertList.push(req.body);
-  res.sendStatus(201);
-});
