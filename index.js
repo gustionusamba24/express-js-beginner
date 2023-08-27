c32090331d141d2fcd9a4c2bf48e4beab96054cc
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-08-27 11:55:59 +0700

    Basic routing

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
